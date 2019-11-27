2d87a2947b749d6dd8aa18e7bcd6816a0f255e9f
Teegroon-on <54004088+Teegroon-on@users.noreply.github.com>
2019-11-27 18:19:40 +0300

    Typ
    End

---
+++ WindowsFormsAppTYp/WindowsFormsAppTYp/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsAppTYp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.buttonCalc = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(220, 93);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "\r\nQR → + | - |QR+|\r\nS→ +| QR+\r\n";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label2.Location = new System.Drawing.Point(383, 28);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(232, 25);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Введите цепочку языка";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.textBox1.Location = new System.Drawing.Point(388, 71);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(227, 26);
+			this.textBox1.TabIndex = 2;
+			// 
+			// buttonCalc
+			// 
+			this.buttonCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.buttonCalc.Location = new System.Drawing.Point(416, 115);
+			this.buttonCalc.Name = "buttonCalc";
+			this.buttonCalc.Size = new System.Drawing.Size(161, 31);
+			this.buttonCalc.TabIndex = 3;
+			this.buttonCalc.Text = "Вычислить";
+			this.buttonCalc.UseVisualStyleBackColor = true;
+			this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(388, 153);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(227, 244);
+			this.listBox1.TabIndex = 4;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 115);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(333, 282);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(653, 428);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.buttonCalc);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Lab 1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button buttonCalc;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ WindowsFormsAppTYp/WindowsFormsAppTYp/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppTYp
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonCalc_Click(object sender, EventArgs e)
+		{
+			if (textBox1.Text != "" && textBox1.Text != " ")
+			{
+				const int H = 0, QR = 1, S = 2, ER = 3;
+				int CS = H;
+				string mass = textBox1.Text;
+				for (int i = 0; i < mass.Length && CS != ER; i++)
+				{
+					switch (CS)
+					{
+						case H:
+							{
+								if (mass[i] == '+' && mass.Length == 1)
+								{
+									listBox1.Items.Add(mass + "-->H");
+									listBox1.Items.Add(mass[i] + "-->S");
+									CS = S;
+								}
+								else if ((mass[i] == '+' || mass[i] == '-') && mass.Length > 1)
+								{
+									listBox1.Items.Add(mass + "-->H");
+									listBox1.Items.Add(mass[i] + "-->QR");
+									CS = QR;
+								}
+								else
+								{
+									CS = ER;
+								}
+								break;
+							}
+						case QR:
+							{
+								if (mass[i] == '+' && mass.Length == i)
+								{
+									listBox1.Items.Add(mass[i] + "-->S");
+									CS = S;
+								}
+								else if (mass[i] == '+' && mass.Length > i)
+								{
+									listBox1.Items.Add(mass[i] + "-->QR");
+									CS = QR;
+								}
+								else
+								{
+									CS = ER;
+								}
+								break;
+							}
+					}
+				}
+
+				if (CS != ER)
+				{
+					listBox1.Items.Add("Цепочка  принадлежит");
+					listBox1.Items.Add("языку");
+				}
+				else
+				{
+					listBox1.Items.Add("Цепочка  не принадлежит");
+					listBox1.Items.Add("языку");
+				}
+
+			}
+			else
+			{
+				MessageBox.Show("Для вычисления заполните поле для цепочки языка!");
+			}
+		}
+	}
+}
